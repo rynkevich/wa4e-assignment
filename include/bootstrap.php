f55efd54c5d668e27b91ad30c01e6db7630e5b9b
NRGb3nder <energybender120@gmail.com>
2018-02-18 00:39:16 +0300

    Delete unused css file

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -4,7 +4,3 @@
 
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Custom styles for this template -->
-<link href="starter-template.css" rel="stylesheet">
-
