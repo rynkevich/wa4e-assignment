d85588f23fc7613a3577fb44edcf5f23b2349131
NRGb3nder <energybender120@gmail.com>
2018-03-04 01:33:04 +0300

    Add safe query function

--- include/pdo.php
+++ include/pdo.php
@@ -2,4 +2,13 @@
     $pdo = new PDO('mysql:host=localhost;port=8889;dbname=resume_registry',
         'application', 'Pa$$w0rD');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    function pdo_query_s($query, $substitutions) {
+        global $pdo;
+
+        $stmt = $pdo->prepare($query);
+        $stmt->execute($substitutions);
+
+        return $stmt;
+    }
 ?>
